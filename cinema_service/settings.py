38ad2334fc553423c1bf776242b5e2f6cdf6a973
Vasyl Cherkes <cherkesva@gmail.com>
2025-12-01 00:56:08 +0200

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -35,8 +35,8 @@ INTERNAL_IPS = [
 
 # REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
 }
 
 # Application definition
